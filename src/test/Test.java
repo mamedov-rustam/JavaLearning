24afb2ad7a16df84377c36c0da1ce34b25bfa76f
mamedov-rustam <mamedov.developer@gmail.com>
2015-02-27 15:11:02 +0200

    mistake fixed

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,37 +1,14 @@
 package test;
 
+import java.util.List;
+
 /**
  * Created by Rustam on 27.02.2015.
  */
 public class Test {
+    static String pi = "pipec";
     public static void main(String[] args) {
-        Class1 a  = new Class3();
-        a.print();
-    }
-}
-
-class Class1{
-    public String str = "Parent string";
-
-    public void print(){
-        System.out.println(str);
-    }
-}
-
-class Class2 extends Class1{
-    private String str = "Layer string";
-}
-
-class Class3 extends Class2{
-    public String str = "Child string";
-
-    Class3(){
-        System.out.println(((Class1)this).str);
-        System.out.println(str);
-    }
-
-    @Override
-    public void print(){
-        System.out.println(str);
+        Test test = null;
+        System.out.println(test.pi);
     }
 }
